2e3b2fc7555a08652abfb64c82d9a2437d427353
Opio Emmanuel Omona <emmanuelomona1@gmail.com>
2018-09-13 09:23:04 +0300

    fetch a specific order route added

--- api/app.py
+++ api/app.py
@@ -1,27 +1,33 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello World!" 
+    return "Hello World!"
 
 
 orders = [
     {
-        'username' : 'opix',
-        'item' : '1'
+        'username': 'opix',
+        'item': '1'
     },
     {
-        'username' : 'phy',
-        'item' : '2'
+        'username': 'phy',
+        'item': '2'
     }
 ]
 
 
-@app.route('/v1/orders', methods=['GET'])
+@app.route('/api/v1/orders', methods=['GET'])
 def all_orders():
-    return jsonify({'orders' : orders})
+    return jsonify({'orders': orders})
 
+
+@app.route('/api/v1/orders<int:id>', methods=['GET'])
+def one_order(id):
+    order = [order for order in orders if order['id'] == id]
+    return jsonify({'orders': order[0]})
 
 
 if __name__ == "__main__":

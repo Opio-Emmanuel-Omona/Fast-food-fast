5704ef29a9405e0a9f09b3b7a9172d69eb947f6e
Opio Emmanuel Omona <emmanuelomona1@gmail.com>
2018-09-15 23:42:40 +0300

    linting scores 10/10

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+'''The main application'''
 from flask import Flask, jsonify, request, Response, json
-app = Flask(__name__)
+app = Flask(__name__) #pylint: disable=invalid-name
 
 
-orders = []
+#pylint: disable=missing-docstring
+#pylint: disable=redefined-outer-name
 
-class JsonResponse(Response):
+ORDERS = []
+
+class JsonResponse(Response): #pylint: disable=too-many-ancestors
     def __init__(self, json_dict, status=200):
-        super(JsonResponse, self).__init__(response=json.dumps(json_dict), status=status, mimetype='application/json')
+        super(JsonResponse, self).__init__(response=json.dumps(json_dict),
+                                           status=status, mimetype='application/json')
 
 
 @app.route("/")
@@ -23,35 +28,37 @@ def add():
 
 @app.route('/api/v1/orders', methods=['GET'])
 def all_orders():
-    return jsonify({'orders': orders})
+    return jsonify({'orders': ORDERS})
 
 
-@app.route('/api/v1/orders/<string:id>', methods=['GET'])
-def one_order(id):
-    order = [ordering for ordering in orders if ordering['id'] == id]
+@app.route('/api/v1/orders/<string:order_id>', methods=['GET'])
+def one_order(order_id):
+    order = [ordering for ordering in ORDERS if ordering['id'] == order_id]
     return jsonify({'orders': order[0]})
 
 
 @app.route('/api/v1/orders', methods=['POST'])
 def place_order():
-    order = {'id': request.json['id'], 'username': request.json['username'], 'item': request.json['item']}
-    orders.append(order)
-    return jsonify({'orders': orders})
+    order = {'id': request.json['id'],
+             'username': request.json['username'],
+             'item': request.json['item']}
+    ORDERS.append(order)
+    return jsonify({'orders': ORDERS})
 
 
-@app.route('/api/v1/orders/<string:id>', methods=['PUT'])
-def update_order(id):
-    order = [ordering for ordering in orders if ordering['id'] == id]
+@app.route('/api/v1/orders/<string:order_id>', methods=['PUT'])
+def update_order(order_id):
+    order = [ordering for ordering in ORDERS if ordering['id'] == order_id]
     order[0]['username'] = request.json['username']
     order[0]['item'] = request.json['item']
     return jsonify({'orders': order[0]})
 
 
-@app.route('/api/v1/orders/<string:id>', methods=['DELETE'])
-def delete_order(id):
-    order = [ordering for ordering in orders if ordering['id'] == id]
-    orders.remove(order[0])
-    return jsonify({'orders': orders})
+@app.route('/api/v1/orders/<string:order_id>', methods=['DELETE'])
+def delete_order(order_id):
+    order = [ordering for ordering in ORDERS if ordering['id'] == order_id]
+    ORDERS.remove(order[0])
+    return jsonify({'orders': ORDERS})
 
 
 if __name__ == "__main__":
--- test_api.py
+++ test_api.py
@@ -1,20 +1,19 @@
-import pytest
-import requests
+'''using pytest to test the app'''
 import json
+import pytest
 from app import app
 
 
+#pylint: disable=missing-docstring
+#pylint: disable=redefined-outer-name
 @pytest.fixture
-def client(request):
+def client():
+    '''define a test client'''
     app.config['TESTING'] = True
     test_client = app.test_client()
-
-    def teardown():
-        pass
-    
-    request.addfinalizer(teardown)
     return test_client
 
+#Helper methods for json tests
 def post_json(client, url, json_dict):
     return client.post(url, data=json.dumps(json_dict), content_type='application/json')
 
@@ -27,6 +26,9 @@ def delete_json(client, url):
 def json_of_response(response):
     return json.loads(response.data.decode('utf8'))
 
+#END helper methods
+
+#Tests Begin
 def test_dummy(client):
     response = client.get('/')
     assert b'Hello World!' in response.data
@@ -36,33 +38,37 @@ def test_json(client):
     assert response.status_code == 200
     assert json_of_response(response) == {'answer': 'value' * 2}
 
-def test_getOrders(client):
+def test_get_orders(client):
     response = client.get('/api/v1/orders')
     assert response.status_code == 200
 
-def test_placeOrder(client):
-    response = post_json(client, '/api/v1/orders', {'id': '1', 'username': 'simon', 'item': 'chips'})
+def test_place_order(client):
+    response = post_json(client, '/api/v1/orders',
+                         {'id': '1', 'username': 'simon', 'item': 'chips'})
     assert response.status_code == 200
 
-def test_oneOrder(client):
-    response1 = post_json(client, '/api/v1/orders', {'id': '2', 'username': 'peter', 'item': 'chips + chicken'})
+def test_one_order(client):
+    response1 = post_json(client, '/api/v1/orders',
+                          {'id': '2', 'username': 'peter', 'item': 'chips + chicken'})
     response2 = client.get('/api/v1/orders/1')
     assert response1.status_code == 200
     assert response2.status_code == 200
 
-def test_updateOrder(client):
-    response1 = post_json(client, '/api/v1/orders', {'id': '3', 'username': 'phiona', 'item': 'pork'})
+def test_update_order(client):
+    response1 = post_json(client, '/api/v1/orders',
+                          {'id': '3', 'username': 'phiona', 'item': 'pork'})
     response2 = put_json(client, '/api/v1/orders/3', {'username': 'phiona', 'item': 'pizza'})
     assert response1.status_code == 200
     assert response2.status_code == 200
 
-def test_deleteOrder(client):
-    response1 = post_json(client, '/api/v1/orders', {'id': '4', 'username': 'wilful', 'item': 'matooke'}) 
+def test_delete_order(client):
+    response1 = post_json(client, '/api/v1/orders',
+                          {'id': '4', 'username': 'wilful', 'item': 'matooke'})
     response2 = delete_json(client, '/api/v1/orders/4')
     assert response1.status_code == 200
     assert response2.status_code == 200
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.config['DEBUG'] = True
     app.config['TESTING'] = True

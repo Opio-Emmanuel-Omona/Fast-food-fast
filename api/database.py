c75eed0cad4faca38b1d4113026f8740b945a365
Opio Emmanuel Omona <emmanuelomona1@gmail.com>
2019-03-11 13:15:07 +0300

    Update database.py

--- api/database.py
+++ api/database.py
@@ -74,14 +74,6 @@ class DatabaseConnection():
         print ("Order table create")
 
     def create_status_table(self):
-        sql0 = (
-            '''
-            DROP TABLE IF EXISTS "status";
-            '''
-        )
-        self.cursor.execute(sql0)
-        self.connection.commit()
-        print ("Status table dropped")
 
         sql1 = (
             '''
